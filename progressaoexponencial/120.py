51ecda586b4c82d86192a06f28d61538dc7a43cb
Ana Paula Santos <60950042+anapsantos013@users.noreply.github.com>
2020-03-03 22:16:47 -0300

    Add files via upload

---
+++ progressaoexponencial/120.py
@@ -0,0 +1,10 @@
+#autores:1dsi3
+#@ana_paula
+#feito em 03/03/2020
+#instituição: etec
+X=0 #declaração contador
+valor=2 #declaração da variável valor
+while (x<=21):
+	valor=valor*valor
+	x=x+1 #laço de repetição
+print=valor #exibir variável valor 
